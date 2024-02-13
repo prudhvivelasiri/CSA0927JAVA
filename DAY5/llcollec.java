d5fd54221e6ad8732034724545621b1bee60cd10
prudhvivelasiri <130492534+prudhvivelasiri@users.noreply.github.com>
2024-02-13 10:03:45 +0530

    Add files via upload

---
+++ DAY5/llcollec.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class llcollec{
+    public static void main(String[]  args){
+        List<String> L=new LinkedList<String>();
+        L.add("ABC");
+        L.add("def");
+        L.add("HNO");
+        System.out.println(L);
+        L.remove("ABC");
+        System.out.println(L);
+
+
+    }
+    }
+
+
